349dc2f2780e39e981ac208e3fdc2f74e1b1e412
MonkeySon <daniel.klepatsch@gmx.at>
2024-01-10 22:21:56 +0100

    Use connect / reconnect mechanism in main program

--- src/HS110EnergyCollector.py
+++ src/HS110EnergyCollector.py
@@ -2,6 +2,7 @@ import sys
 import json
 import traceback
 import time
+import socket
 
 from HS110 import HS110
 from HS110EnergyPoint import HS110EnergyPoint
@@ -29,14 +30,46 @@ hs110 = HS110(cfg['hs110_ip'])
 interval = cfg['interval_sec']
 influxDBCfg = cfg['influxDB']
 
+print('Starting HS110 Energy Collector ...')
+
+print('Connecting ...')
 try:
-    print('Running ...')
-    while True:
+    hs110.connect()
+except Exception as e:
+    print('Exception during initial connection:', e)
+    print(traceback.format_exc())
+    MailifierUtil.mailify_exception('Exception during initial connection')
+    exit(1)
+print('Connected!')
+
+def handle_reconnect(retries=10):
+    for _ in range(retries):
+        try:
+            hs110.connect()
+            return
+        except socket.timeout:
+            print('Reconnect timed out, retrying ...')
+        except Exception as e:
+            print('Exception during reconnect:', e)
+            print(traceback.format_exc())
+            MailifierUtil.mailify_exception('Exception during reconnect')
+            exit(1)
+    print(f'Could not reconnect after {retries} times!')
+    MailifierUtil.mailify('Reconnect error', f'Could not reconnect after {retries} times!')
+    exit(1)
+
+print('Starting to collect energy data ...')
+while True:
+    try:
         energy_json = hs110.get_energy()
         ep = HS110EnergyPoint(energy_json)
         InfluxDBConnector.write_point(influxDBCfg, ep)
         time.sleep(interval)
-except Exception as e:
-    print('Exception during execution:', e)
-    print(traceback.format_exc())
-    MailifierUtil.mailify_exception('Exception during execution')
+    except socket.timeout:
+        print('Socket timed out, reconnecting ...')
+        handle_reconnect()
+    except Exception as e:
+        print('Exception during execution:', e)
+        print(traceback.format_exc())
+        MailifierUtil.mailify_exception('Exception during execution')
+        exit(1)
