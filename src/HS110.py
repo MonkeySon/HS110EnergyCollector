137c1d44641c8c7a03532af4080898fdc602768a
MonkeySon <daniel.klepatsch@gmx.at>
2024-01-10 21:33:07 +0100

    HS110: encrypt commands once at object creation

--- src/HS110.py
+++ src/HS110.py
@@ -10,23 +10,28 @@ class HS110:
         self.ip = ip
         self.port = port
 
-    COMMANDS = {
-        'info'     : '{"system":{"get_sysinfo":{}}}',
-        'on'       : '{"system":{"set_relay_state":{"state":1}}}',
-        'off'      : '{"system":{"set_relay_state":{"state":0}}}',
-        'led_on'   : '{"system":{"set_led_off":{"off":0}}}',
-        'led_off'  : '{"system":{"set_led_off":{"off":1}}}',
-        'state'    : '{"system":{"get_sysinfo":{}}}',
-        'cloudinfo': '{"cnCloud":{"get_info":{}}}',
-        'wlanscan' : '{"netif":{"get_scaninfo":{"refresh":0}}}',
-        'time'     : '{"time":{"get_time":{}}}',
-        'schedule' : '{"schedule":{"get_rules":{}}}',
-        'countdown': '{"count_down":{"get_rules":{}}}',
-        'antitheft': '{"anti_theft":{"get_rules":{}}}',
-        'reboot'   : '{"system":{"reboot":{"delay":1}}}',
-        'reset'    : '{"system":{"reset":{"delay":1}}}',
-        'energy'   : '{"emeter":{"get_realtime":{}}}'
-    }
+        # Available commands in cleartext
+        self.COMMANDS = {
+            'info'     : '{"system":{"get_sysinfo":{}}}',
+            'on'       : '{"system":{"set_relay_state":{"state":1}}}',
+            'off'      : '{"system":{"set_relay_state":{"state":0}}}',
+            'led_on'   : '{"system":{"set_led_off":{"off":0}}}',
+            'led_off'  : '{"system":{"set_led_off":{"off":1}}}',
+            'state'    : '{"system":{"get_sysinfo":{}}}',
+            'cloudinfo': '{"cnCloud":{"get_info":{}}}',
+            'wlanscan' : '{"netif":{"get_scaninfo":{"refresh":0}}}',
+            'time'     : '{"time":{"get_time":{}}}',
+            'schedule' : '{"schedule":{"get_rules":{}}}',
+            'countdown': '{"count_down":{"get_rules":{}}}',
+            'antitheft': '{"anti_theft":{"get_rules":{}}}',
+            'reboot'   : '{"system":{"reboot":{"delay":1}}}',
+            'reset'    : '{"system":{"reset":{"delay":1}}}',
+            'energy'   : '{"emeter":{"get_realtime":{}}}'
+        }
+
+        # Encrypt commands for direct usage
+        for cmd_name in self.COMMANDS.keys():
+            self.COMMANDS[cmd_name] = self._encrypt(self.COMMANDS[cmd_name])
 
     def _encrypt(self, string):
         key = 0xAB
@@ -48,45 +53,45 @@ class HS110:
         plaintext = bytes(buffer)
         return plaintext.decode()
 
-    def _write_command(self, command):
+    def _write_command(self, encrypted_command):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((self.ip, self.port))
-        sock.send(self._encrypt(command))
+        sock.send(encrypted_command)
         data = sock.recv(4096)
         sock.close()
         return self._decrypt(data[4:])
 
     def get_info(self):
-        data = self._write_command(HS110.COMMANDS['info'])
+        data = self._write_command(self.COMMANDS['info'])
         json_data = json.loads(data)
         return json_data
 
     def get_time(self):
-        data = self._write_command(HS110.COMMANDS['time'])
+        data = self._write_command(self.COMMANDS['time'])
         json_data = json.loads(data)
         return json_data
 
     def get_energy(self):
-        data = self._write_command(HS110.COMMANDS['energy'])
+        data = self._write_command(self.COMMANDS['energy'])
         json_data = json.loads(data)
         return json_data
 
     def set_on(self):
-        data = self._write_command(HS110.COMMANDS['on'])
+        data = self._write_command(self.COMMANDS['on'])
         json_data = json.loads(data)
         return json_data
 
     def set_off(self):
-        data = self._write_command(HS110.COMMANDS['off'])
+        data = self._write_command(self.COMMANDS['off'])
         json_data = json.loads(data)
         return json_data
 
     def set_led_off(self):
-        data = self._write_command(HS110.COMMANDS['led_off'])
+        data = self._write_command(self.COMMANDS['led_off'])
         json_data = json.loads(data)
         return json_data
 
     def set_led_on(self):
-        data = self._write_command(HS110.COMMANDS['led_on'])
+        data = self._write_command(self.COMMANDS['led_on'])
         json_data = json.loads(data)
         return json_data
